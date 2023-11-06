945ffe7559afd13bf9ad3e69d142d4ee8cfb578f
jasonhero <jasonhero858@gmail.com>
2023-11-05 23:42:37 -0800

    gui helper spawner

--- run.ts
+++ run.ts
@@ -4,6 +4,7 @@ import { CharStream, CommonTokenStream }  from 'antlr4';
 import { PostgreSQLLexer } from './__generated__/PostgreSQLLexer';
 import { PostgreSQLParser } from './__generated__/PostgreSQLParser'
 import { PostgreSQLToKyselyVisitor } from './src/PostgreSQLToKyselyVisitor'
+import { ChildProcessWithoutNullStreams, spawn, spawnSync } from 'child_process'
 
 const visitor = new PostgreSQLToKyselyVisitor();
 
@@ -17,25 +18,47 @@ function convertSQLToKysely(inputSQL: string): string {
   return kyselyCode
 }
 
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-
+let childProcess: ChildProcessWithoutNullStreams
 
 function getUserInput() {
   rl.question('Enter SQL query: ', (userInput) => {
     if (userInput.toLowerCase() === 'exit') {
+      childProcess?.kill()
       rl.close();
-    } else {
-      console.log('\n-------------\n\n')
-      console.log(convertSQLToKysely(userInput.trim()))
-      console.log('\n\n-------------')
-      getUserInput();
+    } else {    
+      if (childProcess !== undefined) {
+        childProcess.kill('SIGINT')
+      }
+      const _input = userInput.trim()
+
+      try {
+        const kyselyCode = convertSQLToKysely(_input)
+        console.log('\n-------------\n\n')
+        console.log(kyselyCode)
+        console.log('\n\n-------------')
+        
+        // Spawns GUI for visualizing the parsed AST of the provided query
+        childProcess = spawn('antlr4-parse', ['PostgreSQLParser.g4', 'PostgreSQLLexer.g4', 'stmt', '-gui']);
+        childProcess.on('spawn', () => {
+          console.log('\n.... spawning GUI \n')
+          childProcess.stdin.write(_input)
+          childProcess.stdin.write('\n')
+          setTimeout(() => {
+            childProcess.stdin.end()
+            getUserInput();
+          }, 100)
+        }) 
+      } catch (err) {
+        console.log("bad query... try again")
+        getUserInput()
+      }     
     }
   });
 }
 
-getUserInput();
+getUserInput()
